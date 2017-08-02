61a1a429087a4cb58ba021674e65a4dd62c76359
Scott Schmidt <scott.schmidt1001@gmail.com>
2017-08-02 16:57:11 -0500

    work..got docs to run..had to edit server.py baseUrl

--- tutorial/server.py
+++ tutorial/server.py
@@ -10,6 +10,7 @@ import os
 
 server = Flask(__name__)
 server.secret_key = os.environ.get('secret_key', 'secret')
-app = Dash(__name__, server=server, url_base_pathname='/dash/', csrf_protect=False)
+# app = Dash(__name__, server=server, url_base_pathname='/dash/', csrf_protect=False)
+app = Dash(__name__, server=server,  csrf_protect=False)
 
 app.config.supress_callback_exceptions = True
